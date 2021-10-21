c0d011c9ebc89ede250f21c2bd8b3d0540b68ebb
MarkKolyadich <70436486+Garmonik@users.noreply.github.com>
2021-10-21 13:32:39 +0300

    Delete 2 week directory

--- 2 week/1.Factorial/main.cpp
+++
@@ -1,7 +0,0 @@
-int Factorial(int num){
-	if (num <= 1)
-		return 1;
-	else{
-		return num * Factorial(num - 1);
-	}
-}
--- 2 week/2.Palindrom/main.cpp
+++
@@ -1,9 +0,0 @@
-bool IsPalindrom(string str){
-	int n = str.size() / 2;
-	bool flag = true;
-
-	for (int i = 0; i <= n; ++i){
-		flag *= (str[i] == str[(str.size() - 1) - i]);
-	}
-	return flag;
-}
